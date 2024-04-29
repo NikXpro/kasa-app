d29be1684929a3e8220c2db415c6caac842732b6
NikX <41104633+NikXpro@users.noreply.github.com>
2024-04-29 17:42:52 +0200

    Add Banner component to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,15 @@
 import { Layout } from "@components/Layout";
+import { Banner } from "@components/Ui";
 
 export const Home = () => {
   return (
     <>
       <Layout pageActive="home">
+        <Banner
+          image="https://source.unsplash.com/1600x900/?nature&compress=true"
+          title="test"
+          brightness={50}
+        />
         <div>
           <h1>Home</h1>
         </div>
