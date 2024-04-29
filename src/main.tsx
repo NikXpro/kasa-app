7679cec741c7a0c27277a04d5d26b41159554250
NikX <41104633+NikXpro@users.noreply.github.com>
2024-04-29 17:29:12 +0200

    Add @fontsource/montserrat dependency and add react rooter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import App from "@components/App";
+import "@fontsource/montserrat";
+import { Home } from "@pages/Home";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./index.scss";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
