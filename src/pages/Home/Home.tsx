d524762cf256795592ec6d2e0b6ad82de18691e1
NikX <41104633+NikXpro@users.noreply.github.com>
2024-04-29 21:13:16 +0200

    Add logements list to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,46 @@
 import { Layout } from "@components/Layout";
-import { Banner, Tag, Thumb } from "@components/Ui";
-
+import { Banner, Thumb } from "@components/Ui";
+import "./Home.scss";
 export const Home = () => {
+  const logements = [
+    {
+      id: 1,
+      title: "test",
+      image: "https://source.unsplash.com/1600x900/?bedroom&compress=true",
+      linkUrl: "/test",
+    },
+    {
+      id: 2,
+      title: "test",
+      image: "https://source.unsplash.com/1600x900/?bedroom&compress=true",
+      linkUrl: "/test",
+    },
+    {
+      id: 3,
+      title: "test",
+      image: "https://source.unsplash.com/1600x900/?bedroom&compress=true",
+      linkUrl: "/test",
+    },
+    {
+      id: 4,
+      title: "test",
+      image: "https://source.unsplash.com/1600x900/?bedroom&compress=true",
+      linkUrl: "/test",
+    },
+    {
+      id: 5,
+      title: "test",
+      image: "https://source.unsplash.com/1600x900/?bedroom&compress=true",
+      linkUrl: "/test",
+    },
+    {
+      id: 6,
+      title: "test",
+      image: "https://source.unsplash.com/1600x900/?bedroom&compress=true",
+      linkUrl: "/test",
+    },
+  ];
+
   return (
     <>
       <Layout pageActive="home">
@@ -10,13 +49,16 @@ export const Home = () => {
           title="test"
           brightness={50}
         />
-        <Tag name="test" />
-
-        <Thumb
-          linkUrl="/test"
-          image="https://source.unsplash.com/1600x900/?bedroom&compress=true"
-          title="test"
-        />
+        <div className="logements-list">
+          {logements.map((logement) => (
+            <Thumb
+              key={logement.id}
+              title={logement.title}
+              image={logement.image}
+              linkUrl={logement.linkUrl}
+            />
+          ))}
+        </div>
       </Layout>
     </>
   );
