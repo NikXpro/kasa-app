0d4809a46ebe50887461becbd51507955f9894c6
NikX <41104633+NikXpro@users.noreply.github.com>
2024-04-30 15:16:31 +0200

    Add star icon to Icon component

--- src/components/Ui/Icon/Icon.tsx
+++ src/components/Ui/Icon/Icon.tsx
@@ -2,20 +2,23 @@ import { createElement } from "react";
 
 import arrow from "@assets/icons/arrow.svg?react";
 import logo from "@assets/icons/logo.svg?react";
+import star from "@assets/icons/star.svg?react";
 
 const icons = {
   arrow,
   logo,
+  star,
 };
 
 type IconProps = {
   id: string;
   style?: React.CSSProperties;
+  className?: string;
 };
 
-export function Icon({ id, style }: IconProps) {
+export function Icon({ id, style, className }: IconProps) {
   return createElement(icons[id as keyof typeof icons] || "svg", {
-    className: "icon",
+    className: `icon ${className}`,
     style: style,
   });
 }
