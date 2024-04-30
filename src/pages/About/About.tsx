0cca96fd5fb62488947116a9e7ed6c073030c0b7
NikX <41104633+NikXpro@users.noreply.github.com>
2024-04-30 14:57:00 +0200

    Update About page with Accordion component and content

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -9,20 +9,39 @@ export const About = () => {
           image="https://source.unsplash.com/1600x900/?nature&compress=true"
           brightness={70}
         />
-        <Accordion title="test title">
-          <p>dzdqz dqzd </p>
-          <p>dzdqz dqzd </p>
-          <p>dzdqz dqzd </p>
-          <p>dzdqz dqzd </p>
-          <p>dzdqz dqzd </p>
-        </Accordion>
-        <Accordion title="test title">
-          <p>dzdqz dqzd </p>
-          <p>dzdqz dqzd </p>
-          <p>dzdqz dqzd </p>
-          <p>dzdqz dqzd </p>
-          <p>dzdqz dqzd </p>
-        </Accordion>
+        <div className="information">
+          <Accordion title="Fiabilité">
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale.
+              Les photos sont conformes aux logements, et toutes les
+              informations sont régulièrement vérifiées par nos équipes.
+            </p>
+          </Accordion>
+          <Accordion title="Respect">
+            <p>
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+              comportement discriminatoire ou de perturbation du voisinage
+              entraînera une exclusion de notre plateforme.
+            </p>
+          </Accordion>
+          <Accordion title="Service">
+            <p>
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+              comportement discriminatoire ou de perturbation du voisinage
+              entraînera une exclusion de notre plateforme.
+            </p>
+          </Accordion>
+          <Accordion title="Sécurité">
+            <p>
+              La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+              pour les voyageurs, chaque logement correspond aux critères de
+              sécurité établis par nos services. En laissant une note aussi bien
+              à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
+              que les standards sont bien respectés. Nous organisons également
+              des ateliers sur la sécurité domestique pour nos hôtes.
+            </p>
+          </Accordion>
+        </div>
       </Layout>
     </>
   );
